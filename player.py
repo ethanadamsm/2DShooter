1a5484f198032051375cbd95199e271d63d81542
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-10-17 12:54:04 -0600

    start clamp

--- player.py
+++ player.py
@@ -8,12 +8,26 @@ class Player(character.Character):
 				return False
 		return True
 
+	def clamp(self):
+		if len(self.colblocks):
+			self.fl = self.colblocks[0].getX()
+			self.fr = self.colblocks[0].getX()
+			self.ft = self.colblocks[0].getY()
+		for block in self.colblocks:
+			if block.getX() < self.fl:
+				fl = block.getX()
+			elif block.getX() > self.fr:
+				fr = block.getX()
+
+				
+
 	def update(self, map1):
 		blocks = map1.getBlocks()
 		self.colblocks = []
 		for block in blocks:
 			if logic.Logic().getCollision(self.x, self.y, self.w, self.h, block.getX(), block.getY(), 20, 20):
 				self.colblocks.append(block)
+		self.clamp()
 		if self.getAccelerating():
 			self.vy += self.a
 		else:
