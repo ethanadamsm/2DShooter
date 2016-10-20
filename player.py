758eddf9d6bc00dd1177584038dfa865cad7fab6
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-10-20 14:40:42 -0600

    Player clamp method almost work

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(character.Character):
 				return False
 		return True
 
-	def clamp(self):
+	def clamp(self, l, r, space):
 		fl = ""
 		fr = ""
 		ft = ""
@@ -42,28 +42,36 @@ class Player(character.Character):
 				fb = block.getY()
 		if fl != "":
 			print fl
-			self.x = fl + 5
-			if self.x < fl and self.vx < 0:
-				self.x = fl + 20
+			if self.x < fl + 20 and self.y + 40 > ft + 3 and l:
+				self.x = fl
 				self.vx = 0
-				print self.x + " fasdf"
+				print " fasdf"
+			elif self.x + 20 > fr and self.y + 40 > ft + 3 and r:
+				self.x = fr
+				self.vx = 0
+			if self.y < fb:
+				self.y = fb - 39
+				self.vy = 0
 
-	def update(self, map1):
+	def update(self, map1, l, r, space):
 		blocks = map1.getBlocks()
 		self.colblocks = []
 		for block in blocks:
 			if logic.Logic().getCollision(self.x, self.y, self.w, self.h, block.getX(), block.getY(), 20, 20):
 				if block.getType() != "0":
 					self.colblocks.append(block)
-		self.clamp()
 		if self.getAccelerating():
 			self.vy += self.a
 		else:
 			self.vy = 0
-		self.x += self.vx
+		self.clamp(l, r, space)
+		if l:
+			self.x -= 1
+		if r:
+			self.x += 1
 		self.y += self.vy
 
 	def jump(self):
 		self.y -= 20
-		self.vy = -3
+		self.vy = -3 
 	
