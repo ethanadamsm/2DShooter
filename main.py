33d3c6985f1370ca8d9a9b81735e073da3be63b6
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-10-25 12:54:36 -0600

    add click to update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 map1 = gamemap.GameMap("maps/map1.txt")
 player1 = player.Player(200, 0, 20, 40, pygame.image.load("player2unit.png")) 
-player1.addItem(grenade.Grenade(0, 0, 10, 15, pygame.image.load("grenade.png")))
+player1.addItem(grenade.Grenade(0, 0, 10, 15, pygame.image.load("grenade.png"), "grenade"))
 l = False
 r = False
 space = False
@@ -20,7 +20,7 @@ def render():
 	pygame.display.flip()
 
 def update():
-	player1.update(map1, l, r, space)
+	player1.update(map1, l, r, space, click)
 
 while True:
 	render()
