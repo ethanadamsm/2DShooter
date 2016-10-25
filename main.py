2eb2cbff1001fc607cf5bd165ab23cf05ef46155
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-10-25 12:45:03 -0600

    change click var, add item to player

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, pygame, gamemap, player
+import sys, pygame, gamemap, player, grenade
 pygame.init()
 pygame.display.set_caption('2D Shooter')
 
@@ -6,19 +6,21 @@ size = width, height, = 600, 400
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 map1 = gamemap.GameMap("maps/map1.txt")
-player = player.Player(200, 0, 20, 40, pygame.image.load("player2unit.png")) 
+player1 = player.Player(200, 0, 20, 40, pygame.image.load("player2unit.png")) 
+player1.addItem(grenade.Grenade(0, 0, 10, 15, pygame.image.load("grenade.png")))
 l = False
 r = False
 space = False
+click = False
 
 def render():
 	screen.fill(black)
 	map1.render(screen)
-	player.render(screen)
+	player1.render(screen)
 	pygame.display.flip()
 
 def update():
-	player.update(map1, l, r, space)
+	player1.update(map1, l, r, space)
 
 while True:
 	render()
@@ -36,7 +38,7 @@ while True:
 			#if event.key == pygame.K_s:
 				#player.setVelY(1)
 			if event.key == pygame.K_SPACE:
-				player.jump()
+				player1.jump()
 				space = True
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_a:
@@ -45,5 +47,5 @@ while True:
 			if event.key == pygame.K_d:
 				r = False
 			if event.key == pygame.K_w or event.key == pygame.K_s:
-				player.setVelY(0)
+				player1.setVelY(0)
 	update()
